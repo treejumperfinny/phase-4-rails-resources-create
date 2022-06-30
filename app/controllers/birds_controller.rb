58d80140cd4d2f47873bb32b0c7e222cf6c4be9f
Aya Ramirez <ayaramz@gmail.com>
2022-06-29 20:54:58 -0400

    commit tree 17727b58625e27997e874012de5811bb5a5507ab

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -12,8 +12,13 @@ class BirdsController < ApplicationController
     if bird
       render json: bird
     else
-      render json: { error: "Bird not found" }, status: :not_found
+      render json: { error: "Bird cannot be found" }, status: :not_found
     end
   end
+
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end 
 
 end
